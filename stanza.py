a8ca0aa65e5754f4e44eb52e094a2fa15fb38b2b
Jack O'Connor <jackoco@me.com>
2019-11-10 23:09:10 -0500

    cleaned up comments and poetry presentation

--- stanza.py
+++ stanza.py
@@ -1,6 +1,5 @@
 
 
-from PyDictionary import PyDictionary
 from Phyme import Phyme
 import nltk
 import random
@@ -18,32 +17,35 @@ except AttributeError:
 else:
     ssl._create_default_https_context = _create_unverified_https_context
 
-#nltk.download('wordnet')
 from nltk.corpus import wordnet as wordnet
 
 
+"""
+	This function takes a single word and returns a dictionary of dictionaries of "associated" words. 
+	The dictionary returned has keys which are synonyms of the starting word. Each key has a value which
+	is a list of words which are hypernyms or hyponyms of the key. Additionally, the key "antonyms" 
+	refers to a list of antonyms to the starting word. 
+
+"""
 def get_word_web(start_word):
 
 	ph = Phyme()
-
 	love_rhymes = ph.get_perfect_rhymes(start_word)
 
-	list_single = love_rhymes[1];
-	#print(love_rhymes)
-
+	list_single_rhymes = love_rhymes[1];
 	word_web = {}
-	ants = []
+	antonyms = []
 
-	
-	for word in list_single:
+	for word in list_single_rhymes:
 	
 		syns = wordnet.synsets(word)
 		associated_words = {}
-		associated_words["v"] = []
-		associated_words["n"] = []
-		associated_words["s"] = []
-		associated_words["a"] = []
+		associated_words["v"] = [] #verb
+		associated_words["n"] = [] #noun
+		associated_words["s"] = [] 
+		associated_words["a"] = [] #adjective
 		associated_words["r"] = []
+		associated_words["i"] = []
 
 		for l in syns:
 			arr = l.name().split(".")
@@ -64,40 +66,57 @@ def get_word_web(start_word):
 					associated_words[arr[1]] = results
 			for syn in l.lemmas():
 				if syn.antonyms():
-					ants.append(syn.antonyms()[0].name())
+					antonyms.append(syn.antonyms()[0].name())
 		word_web[word] = associated_words
-	word_web["antonyms"] = ants
+	word_web["antonyms"] = antonyms
 	return word_web
 
 
-def make_new_line(diction, word):
+"""
+	This function is what generates new lines for the poem. It is given a word_web 
+	(the dictionary) and the specific key with which it is generating the line according to. 
+	The lines follow a formt which is an article, a noun, a verb, a preposition, and the specific key. 
+	In instances where there are not words in the dictionary to choose from, the system chooses a 
+	generic noun/verb randomly from a list. 
+
+"""
+def make_new_line(dictionary, word):
 
 	v_set = set()
 	n_set = set()
 
 	vowels = {"a", "A", "e", "E", "i", "I", "o", "O", "u", "U"}
 
-
-	random_verbs = ["is", "are", "go", "goes", "has", "have"]
-	random_nouns = ["it", "they", "he", "she"]
-	preps = ["to", "for", "from", "of", "before", "after", "beside", "above", "through", "by"]
-	conjunctions = ["and", "but", "yet"]
+	random_verbs = ["is", "are", "go", "goes", "has", "have", "can", "could", "may"]
+	random_nouns = ["it", "they", "he", "she", "one"]
+	preps = ["to", "for", "from", "of", "before", "after", "beside", "above", "through", "by", "with"]
 
 	verb = ""
 	noun = ""
 	prep = ""
+	adj = ""
 
-	associated_verbs = diction[word]["v"]
-	associated_nouns = diction[word]["n"]
+	associated_verbs = dictionary[word]["v"] 
+	associated_nouns = dictionary[word]["n"]
+	associated_adj = dictionary[word]["a"]
 
-	rand_verb = False
-	rand_noun = False
+	#shuffle to ensure new orders each time this is called for a specific key
+	random.shuffle(associated_nouns)
+	random.shuffle(associated_verbs)
+	random.shuffle(associated_adj)
+
+	#reflects whether or not a generic verb or noun was selected 
+	generic_verb = False
+	generic_noun = False
+
+	if len(associated_adj) > 0:
+		index = random.randrange(0, len(associated_adj), 1)
+		adj = associated_adj[index]
 
 	if len(associated_verbs) == 0:
 		index = random.randrange(0, len(random_verbs), 1)
-		rand_verb = True
+		generic_verb = True
 		verb = random_verbs[index]
-
 	else:
 		index = random.randrange(0, len(associated_verbs), 1)
 		verb = associated_verbs[index]
@@ -105,9 +124,8 @@ def make_new_line(diction, word):
 
 	if len(associated_nouns) == 0:
 		index = random.randrange(0, len(random_nouns), 1)
-		rand_noun = True
+		generic_noun = True
 		noun = random_nouns[index]
-
 	else:
 		index = random.randrange(0, len(associated_nouns), 1)
 		noun = associated_nouns[index]
@@ -115,11 +133,16 @@ def make_new_line(diction, word):
 	index = random.randrange(0, len(preps), 1)
 	prep = preps[index]
 
+
+	#some of the words in the library have underscores instead of spaces
 	verb = verb.replace("_", " ")
 	noun = noun.replace("_", " ")
 
+	if len(associated_adj) > 0:
+		noun = adj + " " + noun
+
 	article = ""
-	if rand_noun == False:
+	if generic_noun == False:
 		char = noun[0]
 		if char in vowels:
 			article = "an"
@@ -131,36 +154,156 @@ def make_new_line(diction, word):
 	else:
 		result = article + " " + noun + " " + verb + " " + prep + " " + word
 
-	if rand_verb == True and rand_noun == True:
+	if generic_verb == True and generic_noun == True:
 		return ""
+
 	return result
 
 
+"""
+	This function calls make_new_line() for each of the keys in the word_web dictionary. 
+	This gives a line to each synonym for the original starting word. 
+"""
 def print_lines(web):
+	lines = []
+	keys = []
 	for key in web.keys():
 		if key != "antonyms":
+			keys.append(key)
+
+	#done to ensure randomness of order
+	random.shuffle(keys)
+
+	for key in keys:
 			string = make_new_line(web, key)
-			print(string)
-			string = "'" + string + "'"
-			os.system("say " + string)
+			if len(string) > 0:
+				lines.append(string)
+	return lines
 
 
+"""
+	This function find the antonym in the list that is least semantically similar to the 
+	given word. It does this by using wordnet's wup_similarity() function which finds a path 
+	from one word to another through synonyms (if possible). 
+
+"""
+def find_best_antonym(ant_list, word):
+	best = 100;
+	best_word = ""
+	original = wordnet.synsets(word)
+	original_syn = wordnet.synset(original[0].name()) #the word we have been given 
+	for ant in ant_list:
+		w1 = wordnet.synsets(ant)
+		antonym_to_compare = wordnet.synset(w1[0].name()) #the antonym we are comparing it to 
+		score = original_syn.wup_similarity(antonym_to_compare)
+		if score != None and score < best:
+			best = score
+			best_word = w1[0].name()
+	return best_word.split(".")[0]
 
 
-def main():
-	web = get_word_web("love")
-	ants = web["antonyms"]
-	print_lines(web)
-	if len(ants) == 0:
-		new_word = "death"
+"""
+	This function takes two lists of lines and combines them into a poem. This function creates
+	12 lines which are comprised of two smaller lines, one from each list. The lines are combined using
+	the word "but" because the lines are created using (loose) antonyms and should have opposing themes. 
+	A list of the 12 final lines is returned. 
+"""
+def synthesize_antonym_components(list1, list2):
+	result = []
+	long_one = []
+	short_one = []
+
+	if len(list1) >= len(list2):
+		long_one = list1
+		short_one = list2
 	else:
-		new_word = ants[0]
-	#web2 = get_word_web(new_word)
-	#print_lines(web2)
-	string = "Thanks for listening"
-	string = "'" + string + "'"
+		long_one = list2
+		short_one = list1
 
-	os.system("say " + string)
+	count = 0;
+	for i in range(0, len(short_one)):
+		string = short_one[i] + " but " + long_one[i]
+		result.append(string)
+		count += 1
+		if count > 11:
+			break
+	return result
+
+
+
+"""	
+	This function evaluates and score a poem on certain criteria. Poems that have greater alliteration, 
+	greater semantic similarities between the first and second parts of each line, respectively, and 
+	greater semantic differences between the first and second halves of each line will receive higher scores. 
+	The final score is simply a sum of each of the subcategories. 
+
+"""
+def evaluate_poem(poem_lines):
+	alliteration_score = 0
+	contradiction_score = 0
+	similarity_score = 0
+
+	for line in poem_lines:
+		first_half = line.split("but")[0]
+		second_half = line.split("but")[1]
+
+		split_words = line.split(" ")
+
+		letter_set = set()
+		for word in split_words:
+			letter_set.add(word[0].lower())
+		alliteration_score += 12 - len(letter_set) #higher score when first letters are repeated 
+
+
+
+	return alliteration_score
+
+
+
+"""
+	This function puts together the above functions, taking an input word as the starting word, 
+	making the word_web associated with it and its best antonym, then synthesizing the lines, and
+	evaluating the poem. The poem will regenerate from the same web until it reaches a score above
+	a certain threshold. Then the function will return that poem 
+	
+"""
+def make_poem(starting_word):
+	start_web = get_word_web(starting_word)
+	start_lines = print_lines(start_web)
+	antonym = find_best_antonym(start_web["antonyms"], starting_word)
+
+	print(starting_word + " vs. " + antonym)
+	ant_web = get_word_web(antonym)
+	ant_lines = print_lines(ant_web)
+
+	total_lines = synthesize_antonym_components(start_lines, ant_lines)
+
+	score = evaluate_poem(total_lines)
+	print("score is " + str(score))
+
+	while score < 50:
+		start_lines = print_lines(start_web)
+		ant_lines = print_lines(ant_web)
+		total_lines = synthesize_antonym_components(start_lines, ant_lines)
+		score = evaluate_poem(total_lines)
+
+	return total_lines
+
+
+
+"""
+	This function calls the function to make the poem with the given word, then prints the poem 
+	and reads the poem aloud. 
+"""
+def main():
+	poem = make_poem("dark")
+
+	count = 0
+	for line in poem:
+		print(line)
+		count += 1
+		if count % 4 == 0:
+			print()
 
 
 main()
